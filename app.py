ee560a87b0ae8df9e73747f813a9bd6441a20da8
marrobriggs <marrobriggs@gmail.com>
2025-08-02 16:23:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-!pip install default-jdk --quiet
 import streamlit as st
 import language_tool_python
 import random
@@ -72,6 +71,7 @@ for sender, message in st.session_state.chat_history:
         st.markdown(f"**🧑 Tu:** {message}")
     else:
         st.markdown(f"**🤖 Bot:** {message}")
+
 
 
 
